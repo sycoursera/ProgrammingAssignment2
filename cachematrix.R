ab0bde89df75dddd08a6ecdde5d4d1791beff50f
sycoursera <sergyashin@yahoo.com>
2015-01-17 03:15:35 +0300

    implemented

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+##########################################################
+# Calculate matrix inverse using caching
+##########################################################
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#---------------------------------------------------------
+# Create a special “vector”, which is really a list containing a functions:
+#   set() the value of the matrix
+#   get() the value of the matrix
+#   setinv() the value of the inverse
+#   getinv() the value of the inverse
+makeCacheMatrix <- function(mat = matrix()) {
+    inv <- NULL
+    set <- function(y) {
+        mat <<- y
+        inv <<- NULL
+    }
+    get <- function() mat
+    setinv <- function(mat_inv) inv <<- mat_inv
+    getinv <- function() inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+#---------------------------------------------------------
+# Calculate inverse using special “vector” created with makeCacheMatrix()
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
+
+
+#---------------------------------------------------------
+# Test
+# hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
+# h8 <- hilbert(8); h8
+# cache = makeCacheMatrix(h8)
+# cacheSolve(cache)
+# cacheSolve(cache)
+# identical(cacheSolve(cache), solve(h8))
